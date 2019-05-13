d7c63f4b0e2baf987dd07a51c668eddd018d2e81
Randolf <zhictory@qq.com>
2019-05-14 02:39:00 +0800

    feat: add rgb and hex

--- snippet.js
+++ snippet.js
@@ -425,5 +425,31 @@
     return newObj;
   }
 }
+{
+  /**
+   * 16 进制颜色值转 RGB
+   * @param {String} hex 16进制颜色字符串
+   * @return {String} RGB颜色字符串
+   */
+  function hexToRGB(hex) {
+    var hexx = hex.replace("#", "0x");
+    var r = hexx >> 16;
+    var g = (hexx >> 8) & 0xff;
+    var b = hexx & 0xff;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+}
+{
+  /**
+   * RGB颜色转16进制颜色
+   * @param  {String} rgb RGB进制颜色字符串
+   * @return {String}     16进制颜色字符串
+   */
+  function RGBToHex(rgb) {
+    var rgbArr = rgb.split(/[^\d]+/);
+    var color = (rgbArr[1] << 16) | (rgbArr[2] << 8) | rgbArr[3];
+    return "#" + color.toString(16);
+  }
+}
 
 module.exports = { getRandom, getUrlParam };
